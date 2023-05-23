f3d952a1b8779824f0210242ec256089104f3aab
Pallavi <120320713+painter98@users.noreply.github.com>
2023-05-23 12:48:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+function drag(event){
+	event.dataTransfer.setData("text", event.target.id);
+}
+
+function allowDrop(event){
+	event.preventDefault();
+}
+
+function drop(event) {
+        event.preventDefault();
+        var fetchData = event.dataTransfer.getData("text");
+        event.target.appendChild(document.getElementById(fetchData));
+    }
